3f09e5b0c77c239c6461fddb19418c3204106949
Tomas Barrera <tbarrera@justiciacordoba.gob.ar>
2022-04-22 13:33:40 -0300

    Se agrega proyecto

---
+++ src/test/java/dev/ppai2022/ppai2022/Ppai2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ppai2022.ppai2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ppai2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
